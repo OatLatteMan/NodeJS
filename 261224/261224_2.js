27f18d0ea773e4fd9ac911a786e8c59a38313d57
Dmitrii Panov <15minstan@gmail.com>
2024-12-27 02:51:39 +0100

    directories managing

---
+++ 261224/261224_2.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('This is actual home page')
+})
+
+app.get('/oatlatteman', (req, res) => {
+    res.send('This is actual page of my GitHub')
+})
+
+app.get('/user/:username/:id', (req, res) => {
+    res.send(`ID: ${req.params.id}. Username: ${req.params.username}`)
+})
+
+const PORT = 3000
+
+app.listen(3000, () => {
+    console.log(`Server is started: http://localhost:${PORT}`)
+})
