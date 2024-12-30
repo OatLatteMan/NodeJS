56d61239120f717ad05ebf875f2acbaae3103971
Dmitrii Panov <15minstan@gmail.com>
2024-12-30 23:46:21 +0100

    static files

---
+++ 301224/301224_2.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/user/:username', (req, res) => {
+    let data = {username: req.params.username, hobbies: ['footbal', 'basketball', 'programming']}
+    res.render('user', data)
+})
+
+app.get('/oatlatteman', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/user/:username/:id', (req, res) => {
+    res.send(`ID: ${req.params.id}. Username: ${req.params.username}`)
+})
+
+const PORT = 3000
+
+app.listen(3000, () => {
+    console.log(`Server is started: http://localhost:${PORT}`)
+})
